bb62c8738a97be790a1d1de8200bc0aa9af5ec26
carlosjanulikdev <carlosjanulikdev@gmail.com>
2023-08-11 20:13:22 -0300

    add inserts to database

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -24,4 +24,16 @@ CREATE TABLE `reba`.`person` (
   UNIQUE KEY `person_unique_fields` (`document_type_id`, `document_number`, `country_id` )
 );
 
+--3. Inserts of countries
+insert into reba.country (id, name) values (1, 'Argentina');
+insert into reba.country (id, name) values (2, 'Brasil');
+insert into reba.country (id, name) values (3, 'Chile');
+insert into reba.country (id, name) values (4, 'Paraguay');
+insert into reba.country (id, name) values (5, 'Uruguay');
+
+--4. Inserts of document types
+insert into reba.document_type (id, description) values (1, 'DNI');
+insert into reba.document_type (id, description) values (2, 'Pasaporte nacional');
+insert into reba.document_type (id, description) values (3, 'Tarjeta de residencia');
+
 
